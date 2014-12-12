13f20e40ee79fc12f99bf07dda14ecefafacb185
Richard C Erben <erben22@mtnaircomputer.net>
2014-12-12 01:51:24 -0700

    Adding support to extract a territory zip file.  Added initial method to parse the extracted output and have a swtich statement setup to parse the files.  Right now, it simply outputs the file type, but now I can call helper methods to do things such as import the MCP's or copy files to appropriate sub-directories.

--- scripts/ManageTerritories.ps1
+++ scripts/ManageTerritories.ps1
@@ -17,6 +17,17 @@
     LASTEDIT: 20141128
 #>
 
+[CmdletBinding()]
+param (
+    [parameter(Mandatory=$false, Position=0)]
+    [string]$territoryRoot = 'C:\Users\rerben\Dropbox\ATCSMon\Downloads\Territories\incoming',
+
+    [parameter(Mandatory=$false, Position=0)]
+    [string]$atcsmonRoot = 'C:\Users\rerben\Dropbox\ATCSMon-Script-Test' 
+)
+
+Import-Module WDAC
+
 ###############################################################################
 # Function to grep input from the pipeline.
 ###############################################################################
@@ -30,16 +41,45 @@ function Expand-ZIPFile([string]$file, [string]$destination)
     }
 }
 
-param($territoryRoot)
+
 
 # Ensure the project root exists.
 
 if (Test-path $territoryRoot)
 {
-    Write-Host "territoryRoot exists: $territoryRoot"
+    $territories = Get-ChildItem -File -Filter '*.zip' -Path $territoryRoot
+
+    foreach ($territory in $territories)
+    {
+        $extractDir = Join-Path -Path $territoryRoot -ChildPath 'extract'
+        New-Item -Force -ItemType Directory -Path $extractDir | Out-Null
+
+        Expand-ZIPFile $territory.FullName $extractDir
+
+        Write-Host "Extracted contents:"
+        $extractedContents = Get-ChildItem -File -Path $extractDir
+
+        foreach ($extractedItem in $extractedContents)
+        {
+            Write-Host "  " $extractedItem
+
+            switch ($extractedItem.Extension)
+            {
+                ".mcp" {Write-Host "     MCP or MDB file"}
+                ".mdb" {Write-Host "     MCP or MDB file"}
+                ".lay" {Write-Host "     Layout file"}
+                ".ini" {Write-Host "     INI file"}
+                ".txt" {Write-Host "     TEXT file"}
+                ".kmz" {Write-Host "     KMZ file"}
+                default {Write-Host "     Unknown file"}
+            }
+        }
+
+        Write-Host "Removing the extract directory..." $extractDir
+        Remove-Item -Force -Recurse -Path $extractDir
+    }
 }
 else
 {
     Write-Host "territoryRoot does not exist: $territoryRoot"
 }
-
