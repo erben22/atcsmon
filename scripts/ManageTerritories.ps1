3a22cfb7af3ee85d3d2f5ae81181d6e14dfcc041
Richard C Erben <erben22@mtnaircomputer.net>
2014-12-12 02:58:44 -0700

    Hacked together, but working version of opening an ATCSMon database, and parsing some entries in the MCP table.  Hard-coded all over right now, but it spits out MCP addresses.

--- scripts/ManageTerritories.ps1
+++ scripts/ManageTerritories.ps1
@@ -41,7 +41,48 @@ function Expand-ZIPFile([string]$file, [string]$destination)
     }
 }
 
+Function Connect-Database([string]$database)
+{
+    Write-Host "Connecting to the database: " $database
 
+    $adOpenStatic = 3
+    $adLockOptimistic = 3
+
+    $objConnection = New-Object -comobject ADODB.Connection
+    $objRecordset = New-Object -comobject ADODB.Recordset
+
+    #$provider = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + $database
+    #$provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\rerben\Dropbox\ATCSMon-Script-Test\atcsdb.mdb"
+    #Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\mydatabase.accdb
+    $provider = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\Users\rerben\Dropbox\ATCSMon-Script-Test\atcsdb.mdb"
+
+    Write-Host "Provider string: " $provider
+
+    $objConnection.Open($provider)
+
+    $objRecordset.Open("Select * from MCP", $objConnection,$adOpenStatic,$adLockOptimistic)
+
+    $objRecordset.MoveFirst()
+
+    do 
+    {
+        $objRecordset.Fields.Item("MCPAddress").Value; 
+        $objRecordset.MoveNext()
+    } until ($objRecordset.EOF -eq $True)
+
+    $objRecordset.Close()
+    $objConnection.Close()
+
+
+    Write-Host "Done with the database operations."
+}
+
+function HandleMCPs([System.IO.FileInfo]$mcpFile)
+{
+    Write-Host "Processing an MCP/MDB file: " $mcpFile.FullName
+
+    Connect-Database $mcpFile.FullName
+}
 
 # Ensure the project root exists.
 
@@ -65,8 +106,8 @@ if (Test-path $territoryRoot)
 
             switch ($extractedItem.Extension)
             {
-                ".mcp" {Write-Host "     MCP or MDB file"}
-                ".mdb" {Write-Host "     MCP or MDB file"}
+                ".mcp" {HandleMCPs $extractedItem}
+                ".mdb" {HandleMCPs $extractedItem}
                 ".lay" {Write-Host "     Layout file"}
                 ".ini" {Write-Host "     INI file"}
                 ".txt" {Write-Host "     TEXT file"}
@@ -83,3 +124,4 @@ else
 {
     Write-Host "territoryRoot does not exist: $territoryRoot"
 }
+
