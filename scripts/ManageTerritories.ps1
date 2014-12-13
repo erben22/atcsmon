1053c1530b3d8744b78000ab5c7b60cce16214b0
R. Cody Erben <erben22@mtnaircomputer.net>
2014-12-13 00:18:40 -0700

    Some updates to dig in deeper into the MCP database and testing getting information out of it.

--- scripts/ManageTerritories.ps1
+++ scripts/ManageTerritories.ps1
@@ -45,33 +45,78 @@ Function Connect-Database([string]$database)
 {
     Write-Host "Connecting to the database: " $database
 
+    # Logic should be something like:
+    #   - Open source database.
+    #   - Ensure the MCP table exists.
+    #   - Open destination datbase.
+    #   - Ensure the MCP table exists.
+
+    #   - For each recordset in source database:
+    #   - Add to destination DB
+    #     - What happens if it already exists?  Overwrite or duplicate?
+
+    #   - Do I need to know each column in the MCP db, or, can I just
+    #     pass in a recordset?
+
+
     $adOpenStatic = 3
     $adLockOptimistic = 3
 
     $objConnection = New-Object -comobject ADODB.Connection
-    $objRecordset = New-Object -comobject ADODB.Recordset
+    #$objRecordset = New-Object -comobject ADODB.Recordset
 
-    #$provider = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + $database
-    #$provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\rerben\Dropbox\ATCSMon-Script-Test\atcsdb.mdb"
-    #Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\mydatabase.accdb
     $provider = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\Users\rerben\Dropbox\ATCSMon-Script-Test\atcsdb.mdb"
 
-    Write-Host "Provider string: " $provider
-
     $objConnection.Open($provider)
+    $adSchemaTables = 20
+    $adSchemaColumns = 4
 
-    $objRecordset.Open("Select * from MCP", $objConnection,$adOpenStatic,$adLockOptimistic)
-
-    $objRecordset.MoveFirst()
+    #$objRecordset = $objConnection.OpenSchema($adSchemaTables)
+    $objRecordset = $objConnection.OpenSchema($adSchemaColumns)
 
     do 
     {
-        $objRecordset.Fields.Item("MCPAddress").Value; 
+        #if ("TABLE" -eq $objRecordset.Fields.Item("TABLE_TYPE").Value)
+        if ("MCP" -eq $objRecordset.Fields.Item("TABLE_NAME").Value)
+        {
+            Write-Host "Table Name: " $objRecordset.Fields.Item("COLUMN_NAME").Value;
+        }             
         $objRecordset.MoveNext()
     } until ($objRecordset.EOF -eq $True)
 
     $objRecordset.Close()
+
     $objConnection.Close()
+
+#Set rstList = cnnDB.OpenSchema(adSchemaTables)
+
+#   ' Loop through the results and print the
+#   ' names and types in the Immediate pane.
+#   With rstList
+#      Do While Not .EOF
+#         If .Fields("TABLE_TYPE") <> "VIEW" Then
+#            Debug.Print .Fields("TABLE_NAME") & vbTab & _
+#               .Fields("TABLE_TYPE")
+#         End If
+#         .MoveNext
+#      Loop
+#   End With
+#   cnnDB.Close
+
+
+    #$objRecordset.Open("Select * from MCP", $objConnection,$adOpenStatic,$adLockOptimistic)
+
+    #$objRecordset.MoveFirst()
+
+    #do 
+    #{
+    #    $objRecordset.Fields.Item("MCPAddress").Value; 
+    #    $objRecordset.MoveNext()
+    #} until ($objRecordset.EOF -eq $True)
+
+    #$objRecordset.Close()
+
+    #$objConnection.Close()
 
 
     Write-Host "Done with the database operations."
