8b86c39104f16135c4ce91b7f503c249064cea59
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2015-11-16 22:09:48 -0700

    Working on extracting a territory file.

--- ruby/atcsmon_territory.rb
+++ ruby/atcsmon_territory.rb
@@ -1,7 +1,23 @@
 class ATCSMonTerritory
   require './read_mdb.rb'
 
-  def extract_territory
-    read_mdb(options[:mdb_file])
+  attr_accessor :territory_path
+
+  def initialize(territory_path)
+    @territory_path = territory_path
   end
+
+  def extract_territory()
+    require 'zip'
+    puts "Extracting territory: #{@territory_path}"
+    dest_file = File.join(ENV['TMPDIR'], 'ATCSMonTerritoryExtract')
+     
+    zipfile = Zip::File.new(@territory_path)
+    zipfile.each_with_index do |entry, index|
+      puts "  entry #{index} is #{entry.name}, size = #{entry.size}, compressed size = #{entry.compressed_size}"
+      entry.extract(dest_file)
+      puts "    Extracted to: #{dest_file}"
+    end
+  end
+
 end
--- ruby/import_atcsmon_territories.rb
+++ ruby/import_atcsmon_territories.rb
@@ -1,20 +1,46 @@
-#===============================================================================
+# Program to import ATCSMon territories and install them to
+# the specified program directory.
+#
+# Author::  R. Cody Erben (mailto:erben22@mtnaircomputer.net)
+
+# Parse command line options.
+def parse_options()
+  require 'optparse'
+
+  options = {}
+  OptionParser.new do |opt|
+    opt.on('--mdb_file MDBFILE') { |o| options[:mdb_file] = o }
+    opt.on('--territoryDir ./directory') { |o| options[:territoryDir] = o }
+  end.parse!
+
+  options
+end
+
+# Find all the territories in the specified directory.
+def find_territories(root_territory_dir)
+
+  files = {}
+
+  if File.directory?("#{root_territory_dir}") && File.exist?(root_territory_dir)
+    files = Dir.glob("#{root_territory_dir}/*.zip")
+  else
+    puts "Invalid directory: #{root_territory_dir}"
+  end
+
+  files
+end
+
 # Main app runner.
-#===============================================================================
-require 'optparse'
 
-options = {}
-OptionParser.new do |opt|
-  opt.on('--mdb_file MDBFILE') { |o| options[:mdb_file] = o }
-  opt.on('--territoryDir ./directory') { |o| options[:territoryDir] = o }
-end.parse!
+require './atcsmon_territory.rb'
 
-puts options
-puts options[:mdb_file]
-puts options[:territoryDir]
+cmdline_options = parse_options()
+puts cmdline_options
 
-if File.directory?("#{options[:territoryDir]}") && File.exist?(options[:territoryDir])
-  #files = Dir.glob("#{:territoryDir}/*.zip")
-  #puts files[]
-  puts "test"
+territory_files = find_territories(cmdline_options[:territoryDir])
+puts territory_files
+
+territory_files.each do |territory_path|
+  territory = ATCSMonTerritory.new(territory_path)
+  territory.extract_territory()
 end
