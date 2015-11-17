bdccf628cc2c8923933ee7a1fe58d7d3d897f317
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2015-11-16 20:57:01 -0700

    Adding a territory.  Trying to parse things.

--- ruby/import_atcsmon_territories.rb
+++ ruby/import_atcsmon_territories.rb
@@ -6,12 +6,15 @@ require 'optparse'
 options = {}
 OptionParser.new do |opt|
   opt.on('--mdb_file MDBFILE') { |o| options[:mdb_file] = o }
-  opt.on('--territory territory.zip') { |o| options[:territory] = o }
+  opt.on('--territoryDir ./directory') { |o| options[:territoryDir] = o }
 end.parse!
 
 puts options
 puts options[:mdb_file]
+puts options[:territoryDir]
 
-require './read_mdb.rb'
-
-read_mdb(options[:mdb_file])
+if File.directory?("#{options[:territoryDir]}") && File.exist?(options[:territoryDir])
+  #files = Dir.glob("#{:territoryDir}/*.zip")
+  #puts files[]
+  puts "test"
+end
